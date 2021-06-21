cc2a5c5076b563cd584b5a2061226bf4d28b145a
Shiv <shivansh0200@gmail.com>
2021-06-21 12:41:24 +0530

    og image fix

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
         <Head />
         <meta property="og:title" content="Shivansh Singh" />
         <meta name="description" property="og:description" content="Built Using Next.js"></meta>
-        <meta name="image" property="og:image" content="https://res.cloudinary.com/dpfcrhcnj/image/upload/v1624258301/Screenshot_2021-06-21_at_12.09.14_PM_adx8o0.png"></meta>
+        <meta name="image" property="og:image" content="[https://res.cloudinary.com/dpfcrhcnj/image/upload/c_scale,w_1300/v1624258301/Screenshot_2021-06-21_at_12.09.14_PM_adx8o0.png]"></meta>
         <meta name="author" content="Shivansh Singh"></meta>
         <body className="font-hind">
           <Main />
